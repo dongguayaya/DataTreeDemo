27cc6d28a0e4c40bd3d538f4171f2f1f40e15eb1
dongguayaya <964775989@qq.com>
2020-05-12 14:20:39 +0800

    修复类重名，增加TestCl测试循环队列和数组队列的时间检测

---
+++ ArrayAndQueue/src/TestCl.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class TestCl {
+    //测试使用q运行opCount个enqueue和dequeue操作所需要的时间，单位：秒
+    private static double testQueue(QueueData<Integer> q,int opCount){
+        long startTime=System.nanoTime();
+        Random random=new Random();
+        for(int i=0;i<opCount;i++){
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for(int i=0;i<opCount;i++){
+            q.dequeue();
+        }
+        long endTime=System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+    }
+    public static void main(String[] args) {
+
+        int opCount=100000;
+
+        ArrayQueue<Integer> arrayQueue=new ArrayQueue<>();
+        double time1=testQueue(arrayQueue,opCount);
+        System.out.println("ArrayQueue,time"+time1+" s");
+
+        LooperQueue<Integer> loopQueue=new LooperQueue<>();
+        double time2=testQueue(loopQueue,opCount);
+        System.out.println("LooperQueue,time"+time2+" s");
+    }
+}
