8784af0c05b4f8252ad3d20933bccc1c37e979f9
dongguayaya <964775989@qq.com>
2020-05-14 10:00:39 +0800

    链表和二分搜索树实现集合并进行检测书本字段时间对比

--- Set/src/Main.java
+++ Set/src/Main.java
@@ -1,14 +1,34 @@
+import java.io.File;
 import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
-        ArrayList<String> words1=new ArrayList<>();
-        FileOperation.readFile("pride-and-prejudice.txt",words1);
-        System.out.println("Total words"+words1.size());
+//    public static void main(String[] args) {
 
+//    }
+    private static double testSet(SetDemo<String> set,String filename){
+        long startTime=System.nanoTime();
+        System.out.println(filename);
+        ArrayList<String> words=new ArrayList<>();
+        if(FileOperation.readFile(filename,words)){
+            System.out.println("Total words:"+words.size());
+            for(String word:words)
+                set.add(word);
+            System.out.println("Total different words"+set.getSize());
+        }
+
+        long endTime=System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+
+    }
+    public static void main(String[] args){
+        String filename="pride-and-prejudice.txt";
         BSTSet<String> set1=new BSTSet<>();
-        for (String word:words1)
-            set1.add(word);
-        System.out.println("different words"+set1.getSize());
+        double time1=testSet(set1,filename);
+        System.out.println("BST Set"+time1+" s");
+        System.out.println();
+        LinkedListSet<String> linkedListSet=new LinkedListSet<>();
+        double time2=testSet(linkedListSet,filename);
+        System.out.println("Linked List Set "+time2+" s");
+
+
     }
 }
