9b4bfc048f18be2595b7d11801bc0c888423f03f
dongguayaya <964775989@qq.com>
2020-05-15 09:57:19 +0800

    实现最大堆

---
+++ MaxHeap/src/ArrayData.java
@@ -0,0 +1,163 @@
+
+//E是类型，泛型
+public class ArrayData<E> {
+    private E[] data;
+    private int size;
+
+    /**
+     *
+     * @param capacity
+     */
+    //构造函数，传入数组的容量capacity构造Array
+    public ArrayData(int capacity){
+        data=(E[]) new Object[capacity];
+        size=0;
+    }
+    //无参数构造函数，默认数组的容量capacity=10
+    public ArrayData(){
+        this(10);
+    }
+    public ArrayData(E[] arr){
+        data=(E[])new Object[arr.length];
+        for(int i=0;i<arr.length;i++)
+            data[i]=arr[i];
+        size=arr.length;
+    }
+    //获得数组中的元素个数
+    public int getSize(){
+        return size;
+    }
+    //获得数组中的容量
+    public int getCapacity(){
+        return data.length;
+    }
+    //判断数组是否为空
+    public boolean isEmpty(){
+        return size==0;
+    }
+    //向所有元素后添加一个新元素O(n)
+    public void addLast(E e){
+        add(size,e);
+    }
+    //在所有元素前添加一个新元素O(n)
+    public void addFirst(E e){
+        add(0,e);
+    }
+
+    public void swap(int i,int j){
+        if(i<0||i>=size||j<0||j>=size)
+            throw new IllegalArgumentException("Index is illegal.");
+        E t=data[i];
+        data[i]=data[j];
+        data[j]=t;
+    }
+
+    //在第index个位置插入一个新元素e
+    public void add(int index,E e){
+        if(size==data.length)
+            resize(2*data.length);
+        if(index<0||index>size)
+            throw  new IllegalArgumentException("Addlist failed,Array is full");
+        for(int i=size-1;i>=index;i--)
+            data[i+1]=data[i];
+
+            data[index]=e;
+            size++;
+
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData=(E[]) new Object[newCapacity];
+        for(int i=0;i<size;i++)
+            newData[i]=data[i];
+        data=newData;
+    }
+
+    //获取index索引位置的元素
+    public E get(int index){
+        if(index<0||index>=size){
+            throw new IllegalArgumentException("Get Failed.Index is illegal");
+
+        }
+        return data[index];
+    }
+    public E getLast(){
+        return get(size-1);
+
+    }
+    public E getFirst(){
+        return get(0);
+    }
+    //修改index索引位置的元素e
+    void set(int index,E e){
+        if(index<0||index>=size){
+            throw new IllegalArgumentException("Get Failed.Index is illegal");
+
+        }
+        data[index]=e;
+    }
+    //查找数组中是否有元素e
+    public boolean contains(E e){
+        for(int i=0;i<size;i++){
+            if(data[i].equals(e))
+                return true;
+        }
+        return false;
+    }
+    //查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(E e){
+        for(int i=0;i<size;i++){
+            if(data[i].equals(e))
+                return i;
+        }
+        return -1;
+
+    }
+    //从数组中删除index位置的元素，返回删除的元素
+    public E remove(int index){
+        if(index<0||index>=size){
+            throw new IllegalArgumentException("Get Failed.Index is illegal");
+
+        }
+        E ret=data[index];
+        for(int i=index+1;i<size;i++){
+            data[i-1]=data[i];
+        }
+        size--;
+        data[size]=null; //loitering objects!=memory leak
+        if(size==data.length/4&&data.length/2!=0)
+            resize(data.length/2);
+        return ret;
+    }
+    //从数组中删除第一个元素，返回删除的元素
+    public E removeFirst(){
+        return remove(0);
+    }
+    //从数组中删除最后一个元素，返回删除的元素
+    public E removeLast(){
+        return remove(size-1);
+    }
+    //从数组中删除e
+    public void removeElement(E e){
+        int index=find(e);
+        if(index!=-1){
+            remove(index);
+        }
+
+    }
+
+
+    @Override
+    public String toString(){
+        StringBuilder res=new StringBuilder();
+        res.append(String.format("Array:size =%d,capacity=%d\n",size,data.length));
+        res.append('[');
+        for(int i=0;i<size;i++){
+            res.append(data[i]);
+            if(i!=size-1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+}
---
+++ MaxHeap/src/MaxHeap.java
@@ -0,0 +1,84 @@
+public class MaxHeap<E extends Comparable<E>>  {
+    private ArrayData<E> data;
+    public MaxHeap(int capacity){
+        data=new ArrayData<>(capacity);
+    }
+    public MaxHeap(){
+        data=new ArrayData<>();
+    }
+
+    //返回堆的元素个数
+    public int size(){
+        return data.getSize();
+    }
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+    //返回完全二叉树的数组表示中，一个索引所表示二点元素的父亲节点的索引
+    private int parent(int index){
+        if(index==0)
+            throw new IllegalArgumentException("index-0 doesn't have parents");
+        return (index-1)/2;
+    }
+
+    //返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+    private int leftChild(int index){
+        return index*2+1;
+    }
+    //返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+    private int rightChild(int index){
+        return index*2+2;
+    }
+    //向堆中添加元素
+    public void add(E e){
+        data.addLast(e);
+        siftUp(data.getSize()-1);
+    }
+    private void siftUp(int k){
+        while(k>0&&data.get(parent(k)).compareTo(data.get(k))<0){
+            data.swap(k,parent(k));
+            k=parent(k);
+        }
+    }
+    public E findMax(){
+        if (data.getSize()==0)
+            throw new IllegalArgumentException("Can not findMax when heap");
+        return data.get(0);
+    }
+    public MaxHeap(E[] arr){
+        data=new ArrayData<>(arr);
+        for(int i=parent(arr.length-1);i>=0;i--)
+            siftDown(i);
+    }
+
+    //取出堆中最大元素
+    public E extractMax(){
+        E ret=findMax();
+        data.swap(0,data.getSize()-1);
+        data.removeLast();
+        siftDown(0);
+
+        return ret;
+    }
+    private void siftDown(int k){
+        while(leftChild(k)<data.getSize()){
+            int j=leftChild(k);
+            if(j+1<data.getSize()&&data.get(j+1).compareTo(data.get(j))>0)
+                j=rightChild(k);
+            //data[j]是左右两个孩子中的最大值
+            if(data.get(k).compareTo(data.get(j))>=0)
+                break;
+            data.swap(k,j);
+            k=j;
+
+        }
+    }
+    //取出堆中的最大元素，并且替换成元素e
+    public E replace(E e){
+        E ret=findMax();
+        data.set(0,e);
+        siftDown(0);
+        return ret;
+    }
+
+}
