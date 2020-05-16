b4beafcd57fc2959e4df0ca70685dff6c39a58c6
dongguayaya <964775989@qq.com>
2020-05-16 11:19:44 +0800

    对比比较并查集经过成树成链表添加size进行优化比较并编写测试用例

--- UnionFind/src/Main.java
+++ UnionFind/src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class Main {
+    private static double testUF(UF uf,int m){
+
+        int size=uf.getSize();
+        Random random=new Random();
+        long startTime=System.nanoTime();
+        for(int i=0;i<m;i++){
+            int a=random.nextInt(size);
+            int b=random.nextInt(size);
+            uf.unionElements(a,b);
+        }
+        for(int i=0;i<m;i++){
+            int a=random.nextInt(size);
+            int b=random.nextInt(size);
+            uf.isConnected(a,b);
+        }
+        long endTime=System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+    }
+    public static void main(String[] args) {
+        int size=100000;
+        int m=100000;
+        UnionFindQ unionFindQ=new UnionFindQ(size);
+        System.out.println("UnifindQ:"+testUF(unionFindQ,m)+"s");
+        UnionFindU unionFindU=new UnionFindU(size);
+        System.out.println("UnifindU:"+testUF(unionFindU,m)+"s");
+        UnionFindU2 unionFindU2=new UnionFindU2(size);
+        System.out.println("UnifindU:"+testUF(unionFindU2,m)+"s");
     }
 }
